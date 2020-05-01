1a780c0d96293a03f6eea298ab4619e123d51a7a
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2020-05-01 08:21:40 +0000

    words controller added

---
+++ eng_app_word_meaning/app/controllers/words_controller.rb
@@ -0,0 +1,13 @@
+class WordsController < ApplicationController
+  def index
+    @words = Word.all
+  end
+
+  def register
+
+  end
+
+  def get
+
+  end
+end
--- eng_app_word_meaning/config/routes.rb
+++ eng_app_word_meaning/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # JP resource is here "https://railsguides.jp/routing.html"
+  resources :word, only: [:index]
+  get 'words/index'
+  get 'words/:sentence_no', to: 'words#get'
+  post 'words/:sentence_no', to: 'words#register'
 end
