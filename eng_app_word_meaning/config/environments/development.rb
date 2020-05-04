de97c5047ba35f22bf7dc155f8b131bc4017a071
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2020-05-04 18:01:44 +0000

    environments/development.rb config.hosts.clear

--- eng_app_word_meaning/config/environments/development.rb
+++ eng_app_word_meaning/config/environments/development.rb
@@ -63,5 +63,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.hosts << "miyakz1192.ddns.net"
+  config.hosts.clear
 end
